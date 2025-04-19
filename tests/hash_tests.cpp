100828192cfd189985cefa1418c856444b8b9b0f
noakantor <kantor.noa@gmail.com>
2025-04-19 18:00:09 +0300

    Update hash_tests.cpp

--- tests/hash_tests.cpp
+++ tests/hash_tests.cpp
@@ -1,1 +1,75 @@
+#include "gtest/gtest.h"
+#include "../src/hash/IterativeStdHash.h"
+#include <limits>
 
+using namespace std;
+
+// ----------- Sanity Tests -----------
+TEST(IterativeStdHashTests, ConstructorInitializeFields) {
+    IterativeStdHash iterHash1(1);
+    IterativeStdHash iterHash2(2);
+    IterativeStdHash iterHash5(5);
+
+    // hash class get name should return the name in this format
+    EXPECT_EQ(iterHash1.getName(), "std hash 1");
+    EXPECT_EQ(iterHash2.getName(), "std hash 2");
+    EXPECT_EQ(iterHash5.getName(), "std hash 5");
+}
+
+// same input to the hash function should always!! return the same output (deterministic function)
+TEST(IterativeStdHashTests, SameInputSameHashOutput) {
+    IterativeStdHash iterHash(2);
+    EXPECT_EQ(iterHash("aa"), iterHash("aa"));
+}
+
+// activating the same hash function on different inputs, should usually yield different output (not always!)
+TEST(IterativeStdHashTests, DifferentInputDifferentHashOutput) {
+    IterativeStdHash iterHash(2);
+    EXPECT_NE(iterHash("aa"), iterHash("bb"));
+}
+
+// Creating an iterative std hash function pbject with the value 1 - should return the same output as the regular std hash
+TEST(IterativeStdHashTests, IterationOneEqualsStdHash) {
+    IterativeStdHash iterHash(1);
+    std::hash stdHash;
+    EXPECT_EQ(iterHash("aa"), stdHash("aa"));
+}
+
+// ----------- Negative tests -----------
+// Trying to create an instance of iterative std hash with 0 iterations should return invalid argument
+TEST(IterativeStdHashTests, HandleZeroIterations) {
+    EXPECT_THROW(IterativeStdHash iterHash(0), invalid_argument);
+}
+
+// ----------- Boundary tests -----------
+// empty string should return a value without throwing an error
+TEST(IterativeStdHashTests, HandlesEmptyString) {
+    IterativeStdHash iterHash(2);
+    EXPECT_NO_THROW(iterHash(""));
+}
+
+// long string should return a value without throwing an error
+TEST(IterativeStdHashTests, LongStringHash) {
+    IterativeStdHash iterHash(2);
+    std::string longInput(1024, 'x');
+    EXPECT_NO_THROW(iterHash(longInput));
+}
+
+// large number of iterations should return a value without throwing an error
+TEST(IterativeStdHashTests, LongStringHash) {
+    IterativeStdHash iterHash(100000);
+    EXPECT_NO_THROW(iterHash("Hello"));
+}
+
+// should handle maximizing the number of iterations without crushing
+TEST(IterativeStdHashTests, LongStringHash) {
+    size_t max_size = std::numeric_limits<size_t>::max();
+    IterativeStdHash iterHash(max_size);
+    EXPECT_NO_THROW(iterHash("World"));
+}
+
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS
+}
