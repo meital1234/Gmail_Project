179f584349e0d17f93814764cdb728667c36716d
noakantor <kantor.noa@gmail.com>
2025-05-27 16:27:46 +0300

    created new files and fixed app.js

--- src/web_server/app.js
+++ src/web_server/app.js
@@ -4,12 +4,15 @@ const app = express();
 // Imports the user and token routers from the routes folder.
 const usersRouter = require('./routes/users');
 const tokensRouter = require('./routes/tokens');
+// Import mails router
+const mailsRouter = require('./routes/mails')
 
 app.use(express.json()); // Allows the application to parse JSON requests and read the request body (req.body).
 
-// Connects the routers to the /api/users and /api/tokens paths.
+// Connects the routers to the /api/users and /api/tokens and /api/mails paths.
 app.use('/api/users', usersRouter);
 app.use('/api/tokens', tokensRouter);
+app.use('api/mails', mailsRouter)
 
 const PORT = 8080;
 app.listen(PORT, () => {
