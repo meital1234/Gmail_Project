e32b3fc992c02d3e82c36f3e8dd3a8dd4380a78f
noakantor <kantor.noa@gmail.com>
2025-07-22 16:17:06 +0300

    spam working!!!!

--- src/web_server/models/blacklist.js
+++ src/web_server/models/blacklist.js
@@ -39,8 +39,12 @@ async function isBlacklisted(url) {
     // sent GET to check if wxists
     const responseLine = await TCP.sendCommand(`GET ${url}`);
     console.log(`[BlacklistModel] isBlacklisted("${url}") response:`, responseLine);
-    const code = Number(responseLine.match(/^\d{3}/)?.[0] || 0);
-    return code === 200;  // if exists return 200 OK
+    const lines = responseLine.split(/\r?\n/);
+    const statusLine = lines[0] || '';
+    const code = Number(statusLine.match(/^\d{3}/)?.[0] || 0);
+
+    return code === 200 && lines[2]?.trim() === 'true true';  // explicitly check that second line says "true"
+
   } catch (err) {
     console.error(`[BlacklistModel] isBlacklisted("${url}") error:`, err.message);
     throw new Error("TCP not initialized");
